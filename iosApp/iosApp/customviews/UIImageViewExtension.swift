2e40669a43979fa1d201ad8d26ac61402815788c
KukhtoSlava <kukhto.slava@gmail.com>
2021-04-16 12:18:12 +0300

    LastFm : Add cache for image ios

--- iosApp/iosApp/customviews/UIImageViewExtension.swift
+++ iosApp/iosApp/customviews/UIImageViewExtension.swift
@@ -11,14 +11,20 @@ import UIKit
 extension UIImageView {
     
     func load(url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image = image
+            let cache = URLCache.shared
+            let request = URLRequest(url: url)
+            if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
+                self.image = image
+            } else {
+                URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                    if let data = data, let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300, let image = UIImage(data: data) {
+                        let cachedData = CachedURLResponse(response: response, data: data)
+                        cache.storeCachedResponse(cachedData, for: request)
+                        DispatchQueue.main.async {
+                            self.image = image
+                        }
                     }
-                }
+                }).resume()
             }
         }
-    }
 }
